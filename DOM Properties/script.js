82889e2ed08278ba027c437b8692ee8de2b9b118
wongasor <mddamares.x@gmail.com>
2026-04-09 17:10:33 +0700

    Add files via upload

---
+++ DOM Properties/script.js
@@ -0,0 +1,6 @@
+// Fungsi untuk mengubah warna teks menjadi hijau
+function ubahWarnaHijau() {
+    // Menggunakan DOM property .style.color untuk mengubah warna teks
+    const elemenTeks = document.getElementById("teksSaya");
+    elemenTeks.style.color = "green";
+}
